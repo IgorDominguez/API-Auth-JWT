82a55692fbec61ba2fe7894ddbad86cac378496c
IgorDominguez <igordominguez0079080@gmail.com>
2026-04-28 15:44:01 -0300

    Junção das rotas no swagger principal

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+from api.routes.auth import auth_routes
+from api.routes.user import user_routes
+
+app = FastAPI()
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        # Your OpenAPI document
+        openapi_url=app.openapi_url,
+        # Avoid CORS issues (optional)
+        scalar_proxy_url="https://proxy.scalar.com",
+    )
+
+app.include_router(auth_routes.router)
+app.include_router(user_routes.router)
