40596be13044485bc38b9cb72698e192bab1597d
IgorDominguez <igordominguez0079080@gmail.com>
2026-04-29 14:30:25 -0300

    O comando "uv run main.py" é bem instável para executar o servidor, pois nas vezes que eu testei ele acabou não funcionando em todas ou dando uma falha fantasma, então ele foi removido.

--- src/main.py
+++ src/main.py
@@ -16,7 +16,3 @@ async def scalar_html():
 
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000, reload=True)
